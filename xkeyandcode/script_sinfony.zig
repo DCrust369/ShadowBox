489f7f7727ab892cca338d27a87f9130ce4f2c42
DCrust369 <DCrust@proton.me>
2026-05-21 20:49:21 +0100

    Create script_sinfony.zig

---
+++ xkeyandcode/script_sinfony.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+
+pub fn main ()void {
+    /// testes for ddos or dos 
+    /// atacks
+    .const anuncios: i32 = html(nunber)(ads);
+    .const login: i32 = html(sql)(injection);
+    .const tracker: i32 = shell(scam.rs);
+    .const tracker: i32 = shell(maping-the-project.zig);
+}
+
+continue asm() {
+    .asm tracker: i32 = (maping-the-project.zig);
+    .asm tracker: i8 = (scam.rs);
+}
