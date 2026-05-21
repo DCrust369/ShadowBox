7d8c733ffa65df748c4fba774176934b0f1ac801
DCrust369 <DCrust@proton.me>
2026-05-21 19:15:05 +0100

    Create social-enginer.c

---
+++ xkeyandcode/social-enginer.c
@@ -0,0 +1,33 @@
+#include <studio.h>
+
+/*
+i creat a tool
+for social enginer
+and etical hacking
+and reverse enginer
+*/
+
+continue if () void {
+    const javascript: i16 = (web(apps));
+    const php: i32 = (wed(word-press));
+    const html: i32 = (interface(sites));
+    const css: i32 = (interface(sites(apps)));
+}
+int const brawser = javascriptinpect();
+int const bash_terminal = nano.social.js();
+int const  brawser = HTMLinspect();
+int const bash_terminal = nano_social.html();
+// ok now i build this whith PHP and CSS
+
+continue static Engine-PHP***and***chomium-OR-gecko () {
+    if manipule-DATA () {
+        const(bash(nano_social.php));
+        if else = brawser.nano.php(manipule-code);
+        const(bash(nano_social.css));
+        if else = brawser.nano.php(manipule-code);
+        const PHISHING-atack () {
+            const e-mail: i64 = (proton(tutanota(gmail(outlook))));
+            const e-mail: i32 = exemple.pdf.exe();
+        }
+    }
+}
