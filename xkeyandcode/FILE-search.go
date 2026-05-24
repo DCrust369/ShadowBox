30c25478bb9d92906eae3dd3f1d334167a1cead4
DCrust369 <DCrust@proton.me>
2026-05-24 13:31:55 +0100

    Create FILE-search.go

---
+++ xkeyandcode/FILE-search.go
@@ -0,0 +1,19 @@
+package main 
+
+import (
+	"fmt"
+)
+
+func main () {
+	fmt.Println("protect")
+	const use>> = (spy_reverse_enginer.go)
+	var /spy_reverse_enginer.go int = ghidra
+	var /spy_reverse_enginer.go int = IDA_Pro
+	var /spy_reverse_enginer.go int = Binary_Ninja
+	var /spy_reverse_enginer.go int = wireshark 
+	fmt.Println("i blocker the ghidra and IDA pro and Binary ninja for this tool")
+}
+
+func main () {
+	const use>> = (spy_reverse_enginer.go)
+}
