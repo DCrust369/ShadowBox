6db666f68685490b3310627375c511ce8b390f21
DCrust369 <DCrust@proton.me>
2026-05-27 22:15:35 +0100

    Create sites.c

---
+++ sites.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+// terminal library
+#include <termios.h>
+#include <ioctl.h>
+#include <select.h>
+
+continue int() void {
+    const bash-fish-zsh-powershell: i64 = terminal(terminal-emulator(konsole(pseudo-tele-type(tele-type))));
+    const scam-ip-site-url: i16 = www.shodan.io;
+    const github-codes: i16 = www.grep.app;
+    const brawser-on-terminal: i8 = Lynx;
+}
