339a00c95cc92ab49e714b6fda60b8e1fbb8f9d2
DCrust369 <DCrust@proton.me>
2026-05-28 20:41:27 +0100

    Create interface_terminal.sh

---
+++ Terminal/interface_terminal.sh
@@ -0,0 +1,43 @@
+echo "
+_____________________________________________________
+
+Are you okay
+Are you okay
+Are you okay, $USER
+...
+You've been hit by, you've
+been hit by
+A smooth CyberCriminal
+_____________________________________________________
+                                  \  /
+                                   \/                            
+                                  ________________
+                                 |                |
+                             ____|________________|_____
+                                 |               |
+                                 |               |
+                                 |               |
+                                 |               |
+                                 |               |
+                                 |_______________|   
+                                   /         /
+                   _____ _________/_________/_______________
+                  /     /                            /     /     Open the console in terminal
+                 /     /                            /     /      
+                /     /                            /     /       tools > 
+               /     /                            /     /        > social enginer
+              /     /                            /     /         > maping the project
+             /_____/                            /     /          > scam (created in rust language)
+            |___  /                            /_____/           > virtualization (you use a virtualbox or qemu or docker or gnome box?)
+                 /                            / ____/            > sites.c
+                /                            /                   > sites.sh
+               /________________________    /
+              /     /                /     / 
+             /     /                /     /
+            /     /                /     /
+           /     /                /     /
+          /     /                /     /
+         /     /                /     /
+        /     /                /     / 
+       /_____/                /_____/
+"
